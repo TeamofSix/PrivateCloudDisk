d4e2aa267ed2811c784e413c7d42823e10a0ebf1
Neil Lee <cn.neillee@gmail.com>
2016-10-23 18:14:46 +0800

    通过公网测试

--- src/server/UDPClientA.java
+++ src/server/UDPClientA.java
@@ -7,50 +7,94 @@ import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 
 public class UDPClientA {
-	public static void main(String[] args){
-		try{
-			//向server发起请求
-			SocketAddress target = new InetSocketAddress("210.41.96.172",2008);
+	public static void main(String[] args) {
+		try {
+			// 向server发起请求
+			SocketAddress target = new InetSocketAddress("115.159.34.11", 2008);
 			DatagramSocket client = new DatagramSocket();
 			String message = "I am UDPClientA 192.168.85.132";
 			byte[] sendbuf = message.getBytes();
-			DatagramPacket pack = new DatagramPacket(sendbuf, sendbuf.length,target);
+			DatagramPacket pack = new DatagramPacket(sendbuf, sendbuf.length,
+					target);
 			client.send(pack);
-			//接收请求的回复，可能不是server的回复，可能来自ClientB的请求
+			// 接收请求的回复，可能不是server的回复，可能来自ClientB的请求
 			receive(client);
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	//接收请求内容
-	private static void receive(DatagramSocket client){
-		try{
-			for(;;){
+
+	// 接收请求内容
+	private static void receive(DatagramSocket client) {
+		try {
+			for (;;) {
 				byte[] buf = new byte[1024];
-				DatagramPacket packet = new DatagramPacket(buf,buf.length);
+				DatagramPacket packet = new DatagramPacket(buf, buf.length);
 				client.receive(packet);
-				String receiveMessage = new String(packet.getData(),0,packet.getLength());
+				String receiveMessage = new String(packet.getData(), 0,
+						packet.getLength());
 				System.out.println(receiveMessage);
 				int port = packet.getPort();
 				InetAddress address = packet.getAddress();
 				String reportMessage = "tks";
-				//获取接收到请求内容后并取到地址与端口，然后用获取到的地址与端口回复
-				sendMessage(reportMessage,port,address,client);
+
+				String[] params = receiveMessage.split(",");
+				String Bhost = params[0].substring(5);
+				String Bport = params[1].substring(5);
+
+				// 获取接收到请求内容后并取到地址与端口，然后用获取到的地址与端口回复
+				sendMessage(reportMessage, port, address, client);
+				sendMessage(Bhost, Bport, client);
 			}
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	//回复内容
-	private static void sendMessage(String reportMessage, int port,InetAddress address, DatagramSocket client) {
-		try{
+
+	// 回复内容
+	private static void sendMessage(String reportMessage, int port,
+			InetAddress address, DatagramSocket client) {
+		try {
+			// try{
+			// // DatagramSocket server = new DatagramSocket(6666);
+			// byte[] buf = new byte[1024];
+			// DatagramPacket packet = new DatagramPacket(buf,buf.length);
+			// for(;;){
+			// client.receive(packet);
+			// String receiveMessage = new
+			// String(packet.getData(),0,packet.getLength());
+			// System.out.println(receiveMessage);
+			// }
+			// }catch(Exception e){
+			// e.printStackTrace();
+			// }
 			byte[] sendBuf = reportMessage.getBytes();
-			DatagramPacket sendPacket = new DatagramPacket(sendBuf, sendBuf.length,address,port);
+			DatagramPacket sendPacket = new DatagramPacket(sendBuf,
+					sendBuf.length, address, port);
 			client.send(sendPacket);
-			System.out.println("消息发送成功");
-		}catch(Exception e){
+			System.out.println("消息发送成功!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// 向UDPClientA发起请求(在NAT上打孔)
+	private static void sendMessage(String host, String port,
+			DatagramSocket client) {
+		try {
+			SocketAddress target = new InetSocketAddress(host,
+					Integer.parseInt(port));
+//			for (;;) {
+				String message = "I am clientA count test";
+				byte[] sendbuf = message.getBytes();
+				DatagramPacket pack = new DatagramPacket(sendbuf,
+						sendbuf.length, target);
+				System.out.println(pack);
+				client.send(pack);
+				// 接受UDPClientA的回复
+				receive(client);
+//			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
--- src/server/UDPServer.java
+++ src/server/UDPServer.java
@@ -4,70 +4,89 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
-
+/**
+ * 服务器
+ * @author Neil
+ *
+ */
 public class UDPServer {
-	public static void main(String[] args){
-		try{
+	public static void main(String[] args) {
+		try {
 			DatagramSocket server = new DatagramSocket(2008);
+			
 			byte[] buf = new byte[1024];
-			DatagramPacket packet = new DatagramPacket(buf,buf.length);
-			String sendMessage132 = "";
-			String sendMessage129 = "";
-			int port132 = 0;
-			int port129 = 0;
-			InetAddress address132 = null;
-			InetAddress address129 = null;
-			for(;;){
+			DatagramPacket packet = new DatagramPacket(buf, buf.length);
+			
+			String clientAaddrportInfo = "";
+			String clientBaddrportInfo = "";
+			
+			int portA = 0;
+			int portB = 0;
+			
+			InetAddress addressA = null;
+			InetAddress addressB = null;
+			
+			for (;;) {
 				server.receive(packet);
+
+				String receiveMessage = new String(packet.getData(), 0,
+						packet.getLength());
 				
-				String receiveMessage = new String(packet.getData(),0,packet.getLength());
 				System.out.println(receiveMessage);
-				
-				//接收到clientA
-				if(receiveMessage.contains("132")){
-					port132 = packet.getPort();
-					address132 = packet.getAddress();
-					sendMessage132 = "host:" + address132.getHostAddress() + ",port:" + port132;
+
+				// 接收到clientA
+				if (receiveMessage.contains("UDPClientA")) {
+					portA = packet.getPort();
+					addressA = packet.getAddress();
+					clientAaddrportInfo = "host:" + addressA.getHostAddress()
+							+ ",port:" + portA;
+					System.out.println("Client A:"+clientAaddrportInfo);
 				}
-				
-				//接收到clientB
-				if(receiveMessage.contains("129")){
-					port129 = packet.getPort();
-					address129 = packet.getAddress();
-					sendMessage129 = "host:" + address129.getHostAddress() + ",port:" + port129;
+
+				// 接收到clientB
+				if (receiveMessage.contains("UDPClientB")) {
+					portB = packet.getPort();
+					addressB = packet.getAddress();
+					clientBaddrportInfo = "host:" + addressB.getHostAddress()
+							+ ",port:" + portB;
+					System.out.println("Client B:"+clientBaddrportInfo);
 				}
-				
-				//两个都接收到后分别A、B地址交换互发
-				if(!sendMessage132.equals("") && !sendMessage129.equals("")){
-					send132(sendMessage129,port132,address132,server);
-					send129(sendMessage132,port129,address129,server);
-					sendMessage132 = "";
-					sendMessage129 = "";
+
+				// 两个都接收到后分别A、B地址交换互发
+				if (!clientAaddrportInfo.equals("") && !clientBaddrportInfo.equals("")) {
+					send2A(clientBaddrportInfo, portA, addressA, server);
+					send2B(clientAaddrportInfo, portB, addressB, server);
+					clientAaddrportInfo = "";
+					clientBaddrportInfo = "";
 				}
 			}
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	private static void send129(String sendMessage132,int port132,InetAddress address132,DatagramSocket server){
-		try{
+
+	private static void send2B(String sendMessage132, int port132,
+			InetAddress address132, DatagramSocket server) {
+		try {
 			byte[] sendBuf = sendMessage132.getBytes();
-			DatagramPacket sendPacket = new DatagramPacket(sendBuf, sendBuf.length,address132,port132);
+			DatagramPacket sendPacket = new DatagramPacket(sendBuf,
+					sendBuf.length, address132, port132);
 			server.send(sendPacket);
 			System.out.println("消息发送成功");
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	private static void send132(String sendMessage129,int port129,InetAddress address129,DatagramSocket server){
-		try{
+
+	private static void send2A(String sendMessage129, int port129,
+			InetAddress address129, DatagramSocket server) {
+		try {
 			byte[] sendBuf = sendMessage129.getBytes();
-			DatagramPacket sendPacket = new DatagramPacket(sendBuf, sendBuf.length,address129,port129);
+			DatagramPacket sendPacket = new DatagramPacket(sendBuf,
+					sendBuf.length, address129, port129);
 			server.send(sendPacket);
 			System.out.println("消息发送成功");
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
